1b8c98a5778c8c7133ceb93e57e205991d691a50
Emmanuel BOTROS <emmanuel.botros@gmail.com>
2013-01-27 01:09:37 +0100

    on request tree processing

--- servertree.js
+++ servertree.js
@@ -10,32 +10,6 @@ var includeInThisContext = function(path) {
 }.bind(this);
 
 includeInThisContext("../proctree.js/proctree.js");
-
-var myTree = new window.Tree({
-    "seed": Math.floor((Math.random()*200)+1),
-    "segments": Math.floor((Math.random()*6)+1),
-    "levels": Math.floor((Math.random()*6)+1),
-    "vMultiplier": 2.36,
-    "twigScale": 0.39,
-    "initalBranchLength": 0.49,
-    "lengthFalloffFactor": 0.85,
-    "lengthFalloffPower": 0.99,
-    "clumpMax": 0.454,
-    "clumpMin": 0.404,
-    "branchFactor": 2.45,
-    "dropAmount": -0.1,
-    "growAmount": 2.3,
-    "sweepAmount": 0.01,
-    "maxRadius": 0.139,
-    "climbRate": 0.371,
-    "trunkKink": 0.093,
-    "treeSteps": 5,
-    "taperRate": 0.947,
-    "radiusFalloffRate": 0.73,
-    "twistRate": 3.02,
-    "trunkLength": 1.4
-});
-
 // Create the server.
 http.createServer(function (request, response) {
    // Attach listener on end event.
@@ -48,6 +22,34 @@ http.createServer(function (request, response) {
          'Content-Type': 'text/plain'
       });
       // Send data and end response.
+var seed  = Math.floor((Math.random()*50));
+var myTree = new window.Tree({
+"seed": 810+seed,
+"segments":10,
+"vMultiplier":0.66,
+"twigScale":0.47,
+"initalBranchLength":0.5,
+"lengthFalloffFactor":0.85,
+"lengthFalloffPower":0.99,
+"clumpMax":0.449,
+"clumpMin":0.404,
+"branchFactor":2.75,
+"dropAmount":0.07,
+"growAmount":-0.005,
+"sweepAmount":0.01,
+"maxRadius":0.269,
+"climbRate":0.626,
+"trunkKink":0.108,
+"treeSteps":4,
+"taperRate":0.876,
+"radiusFalloffRate":0.66,
+"twistRate":2.7,
+"trunkLength":1.55,
+});
+console.log (seed);
+
+
+
 // fixing scienfitic notation
 /*var r = myTree.verts.length;
 for (var i =0 ; i <  r; i++) {
